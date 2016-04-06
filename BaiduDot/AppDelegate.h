f76f2ec85c6e8c5fc66b70de0ef4cc487833b0c1
毛健辉 <maojianhui@maojianhuidemacbook-pro.local>
2016-04-06 16:27:20 +0800

    Initial Commit

---
+++ BaiduDot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiduDot
+//
+//  Created by 毛健辉 on 16/4/6.
+//  Copyright © 2016年 Mao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaiduDot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaiduDot
+//
+//  Created by 毛健辉 on 16/4/6.
+//  Copyright © 2016年 Mao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
