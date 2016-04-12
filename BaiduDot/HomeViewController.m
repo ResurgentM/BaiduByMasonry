169c58ed0949c0cb79dcafb665d30d05f8a2f3df
ResurgentM <15727363558@163.com>
2016-04-12 21:42:54 +0800

    second commit

--- BaiduDot/HomeViewController.m
+++ BaiduDot/HomeViewController.m
@@ -8,6 +8,7 @@
 
 #import "HomeViewController.h"
 #import "TableViewCell.h"
+#import <Masonry/Masonry.h>
 @interface HomeViewController ()<UITableViewDataSource,UITableViewDelegate>
 @property(nonatomic,strong)UITableView *tableView;
 @property(nonatomic,strong)UIScrollView *scrollView;
@@ -27,8 +28,8 @@
     [self createSearch];
     [self createTableView];
     [self createScrollView];
-    [self createLabel1];
-    [self createLabel2];
+    [self createView1];
+    [self createView2];
 }
 #pragma make--创建假数据
 -(void)createData{
@@ -38,96 +39,236 @@
 
 }
 -(void)createSearch{
-    UISearchBar *search=[[UISearchBar alloc]initWithFrame:CGRectMake(70, 10, 260, 20)];
-    
-    [self.navigationController.navigationBar addSubview:search];
-    search.placeholder=@"请输入疾病/医生/医院/科室";
-    self.navigationItem.leftBarButtonItem=[[UIBarButtonItem alloc]initWithBarButtonSystemItem:(UIBarButtonSystemItemSearch) target:self action:@selector(toMap:)];
+    UIView *view=[[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 44)];
+    [self.navigationController.navigationBar addSubview:view];
+    UILabel *label=({
+        UILabel *label=[UILabel new];
+        [view addSubview:label];
+        [label mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(view.mas_top).offset(10);
+            make.left.equalTo(view.mas_left).offset(10);
+            make.size.mas_equalTo(CGSizeMake(40, 30));
+        }];
+        
+//        label.backgroundColor=[UIColor redColor];
+        label.text=@"北京";
+        
+        label;
+    });
+    UISearchBar *search=({
+
+        UISearchBar *search=[UISearchBar new];
+        
+        [view addSubview:search];
+        [search mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(view.mas_top).offset(10);
+            make.left.equalTo(label.mas_right).offset(15);
+            make.right.equalTo(view.mas_right).offset(-45);
+            make.size.mas_equalTo(CGSizeMake(260, 30));
+        }];
+        search.placeholder=@"请输入疾病/医生/医院/科室";
+        search;
+    });
+
 
 }
 -(void)createTableView{
-    _tableView=[[UITableView alloc]initWithFrame:CGRectMake(0, 44, self.view.frame.size.width, self.view.frame.size.height-44-49) style:UITableViewStyleGrouped];
-    _tableView.dataSource=self;
-    _tableView.delegate=self;
-    _tableView.contentInset=UIEdgeInsetsMake(360, 0, 0, 0);
-    [_tableView registerNib:[UINib nibWithNibName:@"TableViewCell" bundle:nil] forCellReuseIdentifier:@"cell"];
-    [self.view addSubview:_tableView];
+    _tableView=({
+        UITableView *tableView=[[UITableView alloc]initWithFrame:self.view.frame style:UITableViewStyleGrouped];;
+        [self.view addSubview:tableView];
+        [tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+            
+            make.top.equalTo(self.mas_topLayoutGuide);
+            make.left.equalTo(self.view.mas_left);
+            make.size.equalTo(self.view);
+        }];
+        tableView.delegate=self;
+        tableView.dataSource=self;
+        tableView.contentInset=UIEdgeInsetsMake(340, 0, 0, 0);
+        tableView.showsVerticalScrollIndicator=NO;
+        tableView.contentOffset=CGPointMake(0, -340);
+        [tableView registerNib:[UINib nibWithNibName:@"TableViewCell" bundle:nil] forCellReuseIdentifier:@"cell"];
+        tableView;
+    });
+    
+
 
 }
 -(void)createScrollView{
-    _scrollView =[[UIScrollView alloc]initWithFrame:CGRectMake(0, -360-44, self.view.frame.size.width, 165)];
-    _scrollView.backgroundColor=[UIColor redColor];
-    [_tableView addSubview:_scrollView];
+    _scrollView =({
+        UIScrollView *view=[UIScrollView new];
+        [_tableView addSubview:view];
+        [view mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(_tableView.mas_top).offset(-360-44);
+            make.left.equalTo(_tableView.mas_left);
+//            make.right.equalTo(_tableView.mas_right);
+            make.size.mas_equalTo(CGSizeMake(self.view.frame.size.width, 165));
+        }];
+        view.backgroundColor=[UIColor redColor];
+        
+        view;
+    
+    });
+
 }
--(void)createLabel1{
-    _view1=[[UILabel alloc]initWithFrame:CGRectMake(0, -360-44+165, self.view.frame.size.width, 95)];
-    _view1.userInteractionEnabled=YES;
-    _view1.backgroundColor=[UIColor whiteColor];
-    [_tableView addSubview:_view1];
+-(void)createView1{
+    _view1=({
+        UIView *view=[UIView new];
+        [_tableView addSubview:view];
+        [view mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(_scrollView.mas_bottom);
+            make.left.equalTo(_tableView.mas_left);
+//            make.right.equalTo(_tableView.mas_right);
+            make.size.mas_equalTo(CGSizeMake(self.view.frame.size.width, 95));
+        }];
+        view.userInteractionEnabled=YES;
+        view.backgroundColor=[UIColor whiteColor];
+        
+        view;
+    });
+
     [self createSublabel1];
     
 }
 #pragma make  创建三个子label
 -(void)createSublabel1{
-    NSArray *textArray=@[@"找医院",@"找科室",@"查疾病"];
+        NSArray *textArray=@[@"找医院",@"找科室",@"查疾病"];
+    UIStackView *stackView=[UIStackView new];
+    [_view1 addSubview: stackView];
+    [stackView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(_view1.mas_left).offset(30);
+        make.right.equalTo(_view1.mas_right).offset(-30);
+        make.top.equalTo(_view1.mas_top).offset(10);
+        make.size.height.equalTo(@80);
+        make.size.width.equalTo(@80);
+    }];
+    stackView.axis=UILayoutConstraintAxisHorizontal;
+    stackView.alignment=UIStackViewAlignmentCenter;
+    stackView.distribution=UIStackViewDistributionEqualSpacing;
+    NSArray *array=@[[UIView new],[UIView new],[UIView new]];
     for (int i=0; i<3; i++) {
+        UIView *view=array[i];
+        [view mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.size.mas_equalTo(CGSizeMake(60, 80));
+            
+        }];
+        [stackView addArrangedSubview:view];
         UIButton *button=[[UIButton alloc]init];
-        button.frame=CGRectMake(35+125*i, _view1.frame.origin.y+10, 60, 60);
+        [view addSubview:button];
+        [button mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(view.mas_top);
+            make.left.equalTo(view.mas_left);
+            make.size.mas_equalTo(CGSizeMake(60,60 ));
+        }];
         button.layer.cornerRadius=30;
         button.backgroundColor=[UIColor grayColor];
         button.userInteractionEnabled=YES;
         [button addTarget:self action:@selector(action:) forControlEvents:UIControlEventTouchUpInside];
-        
-        UILabel *subLabel=[[UILabel alloc]initWithFrame:CGRectMake(35+125*i, _view1.frame.origin.y+75, 60, 15)];
+
+        UILabel *subLabel=[UILabel new];
+        [view addSubview:subLabel];
+        [subLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.bottom.equalTo(view.mas_bottom);
+            make.left.equalTo(view.mas_left);
+            make.size.mas_equalTo(CGSizeMake(60, 15));
+        }];
+        subLabel.frame=CGRectMake(0, 65, 60, 15);
         subLabel.text=textArray[i];
-    
-        subLabel.textAlignment=UITextAlignmentCenter;
-        [_tableView addSubview:subLabel];
-        [_tableView addSubview:button];
-//        [_view1 addSubview:label];
-       
-       
+        
+        subLabel.textAlignment=NSTextAlignmentCenter;
+        
     }
+
 }
 #pragma make --常见科室部分
--(void)createLabel2{
-    _view2=[[UILabel alloc]initWithFrame:CGRectMake(0, -360-44+165+95+11, self.view.frame.size.width, 121)];
+-(void)createView2{
+    _view2=[[UIView alloc]initWithFrame:CGRectMake(0, -360-44+165+95+11, self.view.frame.size.width, 121)];
     _view2.backgroundColor=[UIColor whiteColor];
     [_tableView addSubview:_view2];
     //顶部标签
-    UILabel *label=[[UILabel alloc]initWithFrame:CGRectMake(20, _view2.frame.origin.y+10, 80, 25)];
-//    label.backgroundColor=[UIColor blackColor];
+    UILabel *label=[UILabel new];
+    [_view2 addSubview:label];
+    [label mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(_view2.mas_left).offset(20);
+        make.top.equalTo(_view2.mas_top).offset(10);
+        make.size.mas_equalTo(CGSizeMake(80, 25));
+        
+    }];
     label.text=@"常见科室";
-    [_tableView addSubview:label];
+    
     [self createSubLabel2];
    
 }
 -(void)createSubLabel2{
     NSArray *textArray=@[@"内科",@"外科",@"妇产科",@"儿科",@"骨科",@"口腔科",@"眼科",@"更多"];
+    UIStackView *stackView1=({
+        UIStackView *view=[UIStackView new];
+        [_view2 addSubview:view];
+        [view mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(_view2.mas_left).offset(17);
+            make.right.equalTo(_view2.mas_right).offset(-17);
+            make.top.equalTo(_view2.mas_top).offset(35);
+            make.size.mas_equalTo(CGSizeMake(80, 30));
+        }];
+        view;
+    });
+    stackView1.axis=UILayoutConstraintAxisHorizontal;
+    stackView1.alignment=UIStackViewAlignmentCenter;
+    stackView1.distribution=UIStackViewDistributionFillEqually;
+    stackView1.spacing=10;
+    UIStackView *stackView2=({
+        UIStackView *view=[UIStackView new];
+        [_view2 addSubview:view];
+        [view mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(_view2.mas_left).offset(17);
+            make.right.equalTo(_view2.mas_right).offset(-17);
+            make.top.equalTo(stackView1.mas_bottom).offset(10);
+            make.size.mas_equalTo(CGSizeMake(80, 30));
+        }];
+        view;
+    });
+    stackView2.axis=UILayoutConstraintAxisHorizontal;
+    stackView2.alignment=UIStackViewAlignmentCenter;
+    stackView2.distribution=UIStackViewDistributionFillEqually;
+    stackView2.spacing=10;
     for (int i=0; i<8; i++) {
         if (i<4) {
-            UILabel *label=[[UILabel alloc]initWithFrame:CGRectMake(17+88*i, _view2.frame.origin.y+35, 78, 30)];
-            label.backgroundColor=[UIColor colorWithRed:242/255.0 green:247/255.0 blue:255/255.0 alpha:1];
-            label.layer.cornerRadius=2;
-            label.layer.masksToBounds=YES;
-            label.text=textArray[i];
-            label.textAlignment=UITextAlignmentCenter;
-            [_tableView addSubview:label];
+            UILabel *label=({
+                UILabel *label=[UILabel new];
+                [stackView1 addArrangedSubview:label];
+                [label mas_makeConstraints:^(MASConstraintMaker *make) {
+                    make.height.equalTo(@30);
+                }];
+                label.backgroundColor=[UIColor colorWithRed:242/255.0 green:247/255.0 blue:255/255.0 alpha:1];
+                label.layer.cornerRadius=2;
+                label.layer.masksToBounds=YES;
+                label.text=textArray[i];
+                label.textAlignment=NSTextAlignmentCenter;
+                label;
+            });
+            
         }else{
-        
-            UILabel *label=[[UILabel alloc]initWithFrame:CGRectMake(17+88*(i-4), _view2.frame.origin.y+35+40, 78, 30)];
-            label.backgroundColor=[UIColor colorWithRed:242/255.0 green:247/255.0 blue:255/255.0 alpha:1];
-            [_tableView addSubview:label];
-            label.layer.cornerRadius=2;
-            label.layer.masksToBounds=YES;
-            label.text=textArray[i];
-            label.textAlignment=UITextAlignmentCenter;
-            if (i==7) {
-                label.backgroundColor=[UIColor colorWithRed:246/255.0 green:246/255.0 blue:246/255.0 alpha:1];
-            }
-
+            UILabel *label=({
+                UILabel *label=[UILabel new];
+                [stackView2 addArrangedSubview:label];
+                [label mas_makeConstraints:^(MASConstraintMaker *make) {
+                    make.height.equalTo(@30);
+                }];
+                label.backgroundColor=[UIColor colorWithRed:242/255.0 green:247/255.0 blue:255/255.0 alpha:1];
+                label.layer.cornerRadius=2;
+                label.layer.masksToBounds=YES;
+                label.text=textArray[i];
+                label.textAlignment=NSTextAlignmentCenter;
+                if (i==7) {
+                    label.backgroundColor=[UIColor colorWithRed:246/255.0 green:246/255.0 blue:246/255.0 alpha:1];
+                }
+                label;
+            });
         }
+        
+        
     }
+
 
 }
 -(void)toMap:(UIBarButtonItem *)leftButton{
@@ -163,9 +304,19 @@
 -(UIView*)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
     UIView *headerView=[[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 44)];
     headerView.backgroundColor=[UIColor whiteColor];
-    UILabel *label=[[UILabel alloc]initWithFrame:CGRectMake(20, 10, 80, 25)];
-    label.text=@"热门医院";
-    [headerView addSubview:label];
+    UILabel *label=({
+        UILabel *label=[UILabel new];
+        [headerView addSubview:label];
+        [label mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(headerView.mas_left).offset(20);
+            make.top.equalTo(headerView.mas_top).offset(10);
+            make.size.mas_equalTo(CGSizeMake(80, 25));
+        }];
+        label.text=@"热门医院";
+        label;
+    });
+    
+    
     return headerView;
 }
 -(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
